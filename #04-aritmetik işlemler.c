8a315ebf93ebae4274a5002def96257fe3c6c809
emir <89928334+emirhangungormez@users.noreply.github.com>
2022-01-17 03:29:55 +0300

    Rename #4-aritmetik işlemler.c to #04-aritmetik işlemler.c

---
+++ #04-aritmetik işlemler.c
@@ -0,0 +1,14 @@
+// #4: aritmetik işlemler
+
+#include <stdio.h>
+int main()
+{
+	printf("%d\n", 10 + 4);
+	printf("%d\n", 10 - 4);
+	printf("%d\n", 10 * 4);
+	printf("%f\n", 10.0 / 4); // sonucu tam bulmak için noktadan sonrasını belirtmeliyiz.
+	printf("%d\n", 10 % 4); // kalan işlemi.
+
+	getchar();
+	return 0;
+}
